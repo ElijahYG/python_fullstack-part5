66bdd646c39439df330847c0916a378ec76a8595
EY <yanggang0827@hotmail.com>
2017-10-09 22:52:51 +0800

    Create settings.py

---
+++ part5-homework_2/conf/settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+__author__ = "Elijah"
+__date__ = "2017/10/8 13:57"
+
+
+msg_dic = {
+           "group1":
+               {
+                   "client_1": {"IP": "192.168.3.36", "username": "root", "password": "root", "port": 22},
+               },
+           "group2":
+               {
+                   "client_1": {"IP": "192.168.3.37", "username": "root", "password": "root", "port": 22},
+                   "client_2": {"IP": "192.168.3.38", "username": "root", "password": "root", "port": 22},
+               },
+          }
