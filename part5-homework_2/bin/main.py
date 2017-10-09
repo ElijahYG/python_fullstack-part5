fe6d3b97bdaacbfda0e4d52c7ac6ed40529f185c
EY <yanggang0827@hotmail.com>
2017-10-09 22:50:12 +0800

    Create main.py

---
+++ part5-homework_2/bin/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+__author__ = "Elijah"
+__date__ = "2017/10/8 13:57"
+
+import sys
+sys.path.append('../src')
+import core
+
+if __name__ == "__main__":
+    core.run()
